b38838ac706a194c1cbc97b2456fe5deb79f9180
BorisKaryshev <boris.karyshev@gmail.com>
2022-02-08 21:41:55 +0300

    lab 1

---
+++ lab 1/Task 2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("**************\n* C is cool! *\n**************");
+    return 0;
+}
---
+++ lab 1/Task 3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    for (int i = 1; i <= 10; i++){
+        printf("\n");
+        for (int j = 1; j <= 10; j++) {
+            if (j!=1) printf("%4d", i*j);
+            else if (i!=10) printf(" %d", i*j);
+            else printf("%d", i*j);
+        }
+        
+    }
+}
